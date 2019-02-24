aa81e687853c8f1eb3314abbd28a586b10d2b1c8
Gaston Graciani <gastongraciani@gmail.com>
2019-02-23 23:28:56 -0300

    Added featerus

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -36,6 +36,8 @@ var app = new Framework7({
 var mainView = app.views.create('.view-main');
 var jg1 = '';
 var jg2 = '';
+var p1 = 0;
+var p2 = 0;
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function () {
@@ -56,38 +58,93 @@ $$(document).on('page:init', function (e) {
 $$(document).on('page:init', '.page[data-name="anotador"]', function (e) {
   // Do something here when page with data-name="anotador" attribute loaded and initialized
   console.log(e);
+
+
+  // Introduce los nombres del equipo al doom.
   jg1 = $$('#jug1').val();
   jg2 = $$('#jug2').val();
-  $$('#j1').html(jg1);
-  $$('#j2').html(jg2);
+  $$('#jg1').html(jg1);
+  $$('#jg2').html(jg2);
+  var selectElements = document.querySelectorAll('.toque');
 
-  var nro = ['UNO', 'DOS', 'TRES', 'CUATRO', 'CINCO', 'SEIS'];
-
-  function listado() {
-    var aux = [];
-    for (var i = 0; i < nro.length; i++) {
-      console.log(i);
-
-      aux.push(`
-      <article class="row">
-        <h5 class="col-33">${nro[i]}</h5>
-        <input type="text" class="col-33" placeholder="uno" />
-        <input type="text" class="col-33" placeholder="uno" />
-    </article>
-`)
-      $$('#inputs').html();
-
+  // Determina si es el equipo 1 o 2 segun los ids.
+  function equipo(eq) {
+    if (eq % 2 == 0) {
+      return 2;
+    } else {
+      console.log("Equipo 1");
+      return 1;
     }
-
-    for (var i = 0; i < nro.length; i++) {
-      console.log(aux);
-      $$('#inputs').html(aux[i]);
-
-    }
-
   }
 
-  listado();
+  // Multiplica
+  function mult(op, id) {
+    var p = 0
+    var val = id.charAt(0);
+    switch (op) {
+      case '1':
+        console.log(parseInt(val))
+        p = parseInt(val)
+        console.log(p);
+        return p;
+        break;
+      case '2':
+        console.log("Caso 2");
+        p = (parseInt(val) * 2)
+        console.log(p);
+        return p;
+        break;
+      case '3':
+        console.log("Caso 3");
+        p = (parseInt(val) * 3);
+        console.log(p);
+        return p;
+        break;
+      case '4':
+        console.log("Caso 4");
+        p = (parseInt(val) * 4)
+        console.log(p);
+        return p;
+        break;
+      case '5':
+        console.log("Caso 5");
+        p = (parseInt(val) * 5)
+        console.log(p);
+        return p;
+        break;
+      case '6':
+        console.log("Caso 6");
+        p = (parseInt(val) * 6)
+        console.log(p);
+        return p;
+        break;
+    }
+  }
+
+  function changeSelect() {
+    selectElements.forEach((selectElement) => {
+      selectElement.addEventListener('change', () => {
+
+        var id = selectElement.id;
+        var op = id.charAt(0);
+        var val = selectElement.value;
+        console.log(equipo(id));
+        // console.log(mult(val, id))
+
+        if(equipo(id) === 1){
+          p1 += mult(val, id);
+          console.log(p1);
+          $$('#p1').html(p1);
+        } else {
+          p2 += mult(val, id);
+          console.log(p2);
+          $$('#p2').html(p2);
+        }
+      })
+    })
+  }
+  changeSelect();
+
 
 })
 // Option 2. Using live 'page:init' event handlers for each page
